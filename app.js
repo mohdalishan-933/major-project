a520060d90e8bee8e2aa44606d69c1ece6fc00df
Mohd Alishan <mohdalishan933@gmail.com>
2025-08-30 12:18:25 +0530

    Upadte app.js file

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ const listingsRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const Mongo_url ="mongodb://127.0.0.1:27017/wanderlust";
 
-// const dbUrl = process.env.ATLASDB_URL; 
+
+const dbUrl = process.env.ATLASDB_URL; 
 main()
 .then(()=>{
     console.log("Connected to DB");
@@ -33,7 +33,7 @@ main()
     console.log(err);
 })
 async function main() {
-    await mongoose.connect(Mongo_url);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine" ,"ejs");
@@ -44,7 +44,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
-    mongoUrl : Mongo_url,
+    mongoUrl : dbUrl,
     crypto:{
        secret :process.env.SECRET,
     },
